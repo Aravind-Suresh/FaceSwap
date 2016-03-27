ad3c4e55dd2056289068cd5e37687430f76c1160
aravind <arvindsuresh2009@gmail.com>
2016-03-27 22:35:09 +0530

    Teach mask utility done. Fixes #1.

--- read_mask.py
+++ read_mask.py
@@ -4,7 +4,7 @@ import numpy as np
 import os, sys, argparse
 
 idx = 0
-data = {}
+data = []
 input = None
 
 def markPoint(event, x, y, flags, param):
@@ -13,29 +13,28 @@ def markPoint(event, x, y, flags, param):
     global input
 
     if event == cv2.EVENT_LBUTTONUP:
-        data[str(idx)] = [x, y]
+        data.append((x, y))
         cv2.circle(input, (x, y), 3, (0,0,255), 2)
         cv2.putText(input, str(idx), (x, y+4), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 2, cv2.LINE_AA)
         cv2.imshow("Mark points", input)
         idx = idx + 1
 
-        if idx == 68:
-            print data
-            cv2.destroyAllWindows()
-            sys.exit()
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input_image', help='Input image ( Features are extracted )', required=True)
-    parser.add_argument('-p', '--predictor', help='Predictor', required=True)
+    parser.add_argument('-i', '--input_mask_image', help='Input mask image ( Features are extracted )', required=True)
+    parser.add_argument('-o', '--output_mask_data', help='Output mask data path ( *.npy )', required=True)
     args = parser.parse_args()
 
-    input = cv2.imread(args.input_image)
-    shape_predictor = dlib.shape_predictor(args.predictor)
-    face_detector = dlib.get_frontal_face_detector()
+    input = cv2.imread(args.input_mask_image)
 
     cv2.imshow("Mark points", input)
     cv2.setMouseCallback("Mark points", markPoint)
     cv2.waitKey(0)
-    print data
     cv2.destroyAllWindows()
+
+    if idx < 68:
+        print "Insufficient points marked. Aborting.."
+        sys.exit()
+    else:
+        np.save(args.output_mask_data, np.array(data))
+        print "Mask data successfully saved to: " + args.output_mask_data
